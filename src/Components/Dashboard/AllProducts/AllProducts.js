64959f4e847b92193c25822fa83e899cedd676fe
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 20:41:48 +0600

    AllProduct component  updated

--- src/Components/Dashboard/AllProducts/AllProducts.js
+++ src/Components/Dashboard/AllProducts/AllProducts.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
+import ProductsData from '../../../hook/ProductsData/ProductsData';
+import ShowProducts from '../../ShowProducts/ShowProducts';
 
 const AllProducts = () => {
+  const [products] = ProductsData();
   return (
     <div className="px-12 lg:py-12">
       <h2 className='text-3xl py-2 text-primary text-center font-bold'>Our Product</h2>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-10">
+        {products.map((product) => (
+          <ShowProducts
+            product={product}
+            key={product._id}
+          ></ShowProducts>
+        ))}
+      </div>
     </div>
   );
 };
