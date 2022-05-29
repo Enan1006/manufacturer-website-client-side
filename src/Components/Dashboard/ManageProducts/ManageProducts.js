743ac56408b866d82733e87672e0dd3d37f697ea
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 20:44:10 +0600

    manageproduct component  updated

--- src/Components/Dashboard/ManageProducts/ManageProducts.js
+++ src/Components/Dashboard/ManageProducts/ManageProducts.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Products from '../../Home/Product/Products';
+import AllProducts from '../AllProducts/AllProducts';
 
 const ManageProducts = () => {
     return (
         <div>
-
+            <AllProducts></AllProducts>
         </div>
     );
 };
