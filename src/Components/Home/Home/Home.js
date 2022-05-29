986980ecd512a568dd42decc2696c624dc12707e
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 20:52:37 +0600

    Home component  updated

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import Footer from '../../Shared/Footer/Footer';
+import Header from '../../Shared/Header/Header';
+import Banner from '../Banner/Banner';
+import BusinessSummary from '../BusinessSummary/BusinessSummary';
+import HomeContact from '../HomeContact/HomeContact';
+import MakeParts from '../MakeParts/MakeParts';
+import Products from '../Product/Products';
+import Reviews from '../Reviews/Reviews';
+import Summary from '../Summary/Summary';
 
 const Home = () => {
     return (
         <div>
-
+            <Banner></Banner>
+            <Products></Products>
+            <Summary></Summary>
+            <BusinessSummary></BusinessSummary>
+            <MakeParts></MakeParts>
+            <Reviews></Reviews>
+            <HomeContact></HomeContact>
         </div>
     );
 };
