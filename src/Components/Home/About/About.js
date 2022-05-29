96dd1a0762533ec7f245266f9000a2bdb7161029
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 20:49:39 +0600

    Add Review component  updated

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import BusinessSummary from '../BusinessSummary/BusinessSummary';
+import Reviews from '../Reviews/Reviews';
+import Summary from '../Summary/Summary';
 
 const About = () => {
     return (
         <div>
-
+            <BusinessSummary></BusinessSummary>
+            <Summary></Summary>
+            <Reviews></Reviews>
         </div>
     );
 };
