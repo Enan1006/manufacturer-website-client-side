d3cc8d6f89b7b1839f6790036465f24c3f1c88b5
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-05-29 18:31:21 +0600

    not found component updated

--- src/Components/Shared/NotFound/NotFound.js
+++ src/Components/Shared/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import notFound from '../../../Images/not-found.jpg';
 const NotFound = () => {
     return (
-        <div>
-
+        <div className='mt-6 mx-40  object-center'>
+            <img className=' w-100' src={notFound} alt="" />
         </div>
     );
 };
